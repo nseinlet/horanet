f17cd7b1e620437f3f506edc6b58a3e5b0fdf889
Nicolas Seinlet <nse@odoo.com>
2015-11-04 16:13:28 +0100

    Security Sample module

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         'views/course.xml',
         'views/session.xml',
         'views/partner.xml',
--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -8,4 +8,33 @@ class Partner(models.Model):
     
     instructor = fields.Boolean(string='Is instructor')
     session_ids = fields.One2many('openacademy.session', 'instructor_id')
+    type_entite = fields.Selection((('phy', 'Physique'), ('mo', 'Morale')))
+    statut_entite_id = fields.Many2one('openacademy.statutentite')
+    role_mo_ids = fields.One2many('openacademy.rolepersonne', 'partner_mo_id')
+    role_phy_ids = fields.One2many('openacademy.rolepersonne', 'partner_phy_id')
+    
+class StatutEntite(models.Model):
+    _name = 'openacademy.statutentite'
+    
+    name = fields.Char(required=True)
+
+class RoleEntite(models.Model):
+    _name = 'openacademy.role'
+    
+    name = fields.Char(required=True)
+
+class StatutRole(models.Model):
+    _name = 'openacademy.statutrole'
+    
+    name = fields.Char(required=True)
+        
+class RolePersonne(models.Model):
+    _name = 'openacademy.rolepersonne'
+    
+    partner_mo_id = fields.Many2one('res.partner', domain=[('type_entite', '=', 'mo')])
+    partner_phy_id = fields.Many2one('res.partner', domain=[('type_entite', '=', 'phy')])
+    role_id = fields.Many2one('openacademy.role')
+    status_id = fields.Many2one('openacademy.statutrole')
+    start_date = fields.Date()
+    end_date = fields.Date()
     
